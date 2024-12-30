8137438310dc539b87c5d32202b617045880fad2
Mahdi Abbasi <abbasimahdi782@gmail.com>
2024-12-30 10:41:10 +0330

    export prisma service and add DT]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller("auth")
 export class AuthController{
     constructor(private authService: AuthService){}
     
     @Post("signin")
-        signin(){
+        signin(@Body() dto:AuthDto){
+            
+            console.log(dto.email )
             return this.authService.signin();
         }
 
